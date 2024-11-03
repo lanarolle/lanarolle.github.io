cc53fc2e66093c33ae80898235420b4714dabb1d
Raveesha Ramitha <ramithamainframe@gmail.com>
2024-11-04 00:49:29 +0530

    nav

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+    const navToggle = document.getElementById("nav-toggle");
+    const navbar = document.getElementById("navbar-default");
+  
+   
+    navToggle.addEventListener("click", () => {
+      navbar.classList.toggle("hidden");
+    });
+  
